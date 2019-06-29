b1d39e1c53753d2d622916275e23a5e9340b2f1b
dediacc <dedisuhaimiacc@gmail.com>
2019-06-29 16:08:06 +0700

    fix comment

--- regula_falsi.py
+++ regula_falsi.py
@@ -10,7 +10,7 @@ def regula_falsi(a, b, e):
 
     mid = (a*f(b) - b*f(a))/(f(b) - f(a))
 
-    # convergence check using function value error
+    # convergence check
     if abs(a-b) < e:
         return mid
 
