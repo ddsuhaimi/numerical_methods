da22b9431ca71c316088ab1a58bd9d1635ddaded
dediacc <dedisuhaimiacc@gmail.com>
2019-07-04 00:40:49 +0700

    fix bisection

--- bisection_method.py
+++ bisection_method.py
@@ -1,20 +1,18 @@
 def f(x):
-    
+
     return (x-10)*(x-20)*(x+3)
 
 
 def bisection(a, b, e):
 
     if f(a)*f(b) > 0:
-        print(a, b, "input invalid, c")
+        print("input invalid")
         return
-    
-    # convergence check
-    if abs(a-b) < e:
-        result = (a+b)/2
-        return result
 
     mid = (a+b)/2
+
+    if f(mid) == 0:
+        return mid
 
     # change the bracket point
     if f(mid) < 0:
@@ -22,8 +20,13 @@ def bisection(a, b, e):
     else:
         b = mid
 
+    # convergence check
+    if abs(a-b) < e:
+        result = (a+b)/2
+        return result
+
     # recursive call
     return bisection(a, b, e)
 
 
-print(bisection(-4, 2, 1e-15))
+print(bisection(-4, 2, 1e-15))  # -3.0
